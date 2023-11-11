1af5b93f7a69fdd39fbb2de1d92ac8ae5f0b2e17
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-11 17:55:43 +0530

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from './page.module.css'
+
 
 export default function Home() {
   return (
  <div>
-  <h1>Hello</h1>
+   <Link href="/" >Hello</Link>
  </div>
   )
 }
