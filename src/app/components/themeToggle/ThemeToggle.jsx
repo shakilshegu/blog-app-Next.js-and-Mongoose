acb8edb211229d82c5280003ca5ad163c01d244d
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-11 19:25:04 +0530

    new commit

---
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./themeToggle.module.css"
+
+const ThemeToggle = () => {
+  return (
+    <div className={styles.coniainer}>ThemeToggle</div>
+  )
+}
+
+export default ThemeToggle
