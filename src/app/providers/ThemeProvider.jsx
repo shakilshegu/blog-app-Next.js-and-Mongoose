0ebd59ac0ac46ae2f35caebf61b489aac9cbaba8
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-11 20:33:08 +0530

    new commit

--- src/app/providers/ThemeProvider.jsx
+++ src/app/providers/ThemeProvider.jsx
@@ -3,8 +3,6 @@
 import { useContext } from "react"
 import { ThemeContext } from "../context/ThemeContext"
 
-
-
 const ThemeProvider = () => {
     const {theme} = useContext(ThemeContext)
   return (
