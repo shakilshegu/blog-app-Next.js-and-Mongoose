5bbd276aaba535a03633fe43ad7b7f5f00d5be8d
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-12 15:16:04 +0530

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body className={inter.className}>
         <ThemeContextProvider>
           <ThemeProvider>
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,13 +5,13 @@ import Image from 'next/image'
 const Featured = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}><b>Hey , guyyyis</b>Lorem ipsum dolor sit amet consectetur  
-      </h1>
+      <h2 className={styles.title}><b className={styles.bold}>Hey , guyyy </b>is Lorem ipsum dolor sit amet consectetur  
+      </h2>
       <div className={styles.post}>
-        <div className={styles.imgcontainer}>
-          <Image src="/p1.jpeg" alt='' fill />
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt='' fill className={styles.image}/>
         </div>
-        <div className={styles.textcontainer}>
+        <div className={styles.textContainer}>
           <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
           <p className={styles.postDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit id et odit maiores, hic atque!
           </p>
