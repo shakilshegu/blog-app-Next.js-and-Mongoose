658afbcf6aa86301ba63348d20b2e157071f7f29
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-14 11:57:34 +0530

    new commit

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -9,7 +9,7 @@ const Menu = () => {
       <h2 className={styles.subtitle}>{"what's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
       <div className={styles.items}>
-        <Link href="/" className={styles.item}></Link>
+        <Link href="/" className={styles.item}>
         <div className={styles.imageContainer}>
           <Image src="/p1.jpeg" alt="" fill className={styles.image} />
         </div>
@@ -21,6 +21,46 @@ const Menu = () => {
             <span className={styles.date}> - 10.03.2023</span>
           </div>
         </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+          <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur .</h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Jhone doe</span>
+            <span className={styles.date}> - 10.03.2023</span>
+          </div>
+        </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+          <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur .</h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Jhone doe</span>
+            <span className={styles.date}> - 10.03.2023</span>
+          </div>
+        </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+          <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur .</h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Jhone doe</span>
+            <span className={styles.date}> - 10.03.2023</span>
+          </div>
+        </div>
+        </Link>
       </div>
     </div>
   );
